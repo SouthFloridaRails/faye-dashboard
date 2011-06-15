fb2d80cba2e327ea8b85f3ae7582c6f1cdb0dd67
Steve Howell <showell30@yahoo.com>
2011-06-14 18:19:38 -0700

    break out channel and original incoming metadata

--- dashboard.coffee
+++ dashboard.coffee
@@ -1,58 +1,63 @@
 activity_log = ->
   which_msg = 0
-  show_message: (message, type, subscription) ->
+  show_message: (message, type, channel = null) ->
     which_msg += 1
+    tr = $('<tr valign="top">')
+    parent_data = null
+    if message.__parent_data__
+        parent_data = message.__parent_data__
+        message.__parent_data__ = undefined
+        channel = parent_data.channel
+
+    td = $("<td>")
+    td.append(channel)
+    tr.append(td)
     # return if which_msg > 500
     msg = JSON.stringify message, null, 4
-    tr = $('<tr valign="top">')
     td = $("<td>(#{which_msg}) #{type}</td>")
-    tr.append(td)
-    subscription ||= '&nbsp;'
-    td = $("<td>#{subscription}</td>")
     tr.append(td)
     td = $("<td><pre>#{msg}</pre></td>")
     tr.append(td)
+    if parent_data
+      msg = JSON.stringify parent_data, null, 4
+      td = $("<td><pre>#{msg}</pre></td>")
+      tr.append(td)
+
     $("table#log").prepend(tr)
     which_msg
 
 set_up_subscribe_form = (client, activity_logger) ->
   which_subscription = 0
-  on_subcribe = ->
-    try
-      which_subscription += 1
-      channel = $("#subscribe #subscribe_channel").val()
-      subscription_label = "#{which_subscription} #{channel}"
-      subscribe_callback = (message) ->
-        console.log("got message", message)
-        my_subscription = subscription_label
-        activity_logger.show_message(message, 'incoming', my_subscription)
-      console.log("subscribing", channel)
-      subscription = client.subscribe channel, subscribe_callback
-      tr = $('<tr valign="top">')
-      td = $("<td id=subscription#{which_subscription}>")
-      td.append(which_subscription)
-      tr.append(td)
-      td = $("<td>#{channel}</td>")
-      tr.append(td)
-      form = $("<form id='cancel_subscribe'>")
-      button = $("<input type='submit' value='cancel' />")
-      form.append(button)
-      td = $("<td>")
-      td.append(form)
-      tr.append(td)
-      cancel = (subscription, tr) ->
-        ->
-          subscription.cancel()
-          tr.hide()
-          false
-      form.submit(cancel(subscription, tr))
-      $("#subscriptions table").prepend(tr)
-    catch error
-      console.log("error", error)
-    false
+  on_subcribe = (evt) ->
+    evt.preventDefault()
+    which_subscription += 1
+    channel = $("#subscribe #subscribe_channel").val()
+    subscribe_callback = (message) ->
+      console.log("got message", message)
+      activity_logger.show_message(message, 'incoming')
+    console.log("subscribing", channel)
+    subscription = client.subscribe channel, subscribe_callback
+    tr = $('<tr valign="top">')
+    td = $("<td id=subscription#{which_subscription}>")
+    td.append(which_subscription)
+    tr.append(td)
+    td = $("<td>#{channel}</td>")
+    tr.append(td)
+    form = $("<form id='cancel_subscribe'>")
+    button = $("<input type='submit' value='cancel' />")
+    form.append(button)
+    td = $("<td>")
+    td.append(form)
+    tr.append(td)
+    cancel = (subscription, tr) ->
+      (evt) ->
+        evt.preventDefault()
+        subscription.cancel()
+        tr.hide()
+    form.submit(cancel(subscription, tr))
+    $("#subscriptions table").prepend(tr)
 
-  $('form#subscribe').click ->
-    on_subcribe()
+  $('form#subscribe').submit(on_subcribe)
 
 publish = (client, activity_logger) ->
   # singleton
@@ -61,7 +66,7 @@ publish = (client, activity_logger) ->
     data = self.get_input()
     if data?
       channel = $("#publish #channel").val()
-      which_msg = activity_logger.show_message(data, 'outgoing', null)
+      which_msg = activity_logger.show_message(data, 'outgoing', channel)
       console.log("showing", which_msg)
       console.log("publishing", channel, data)
       client.publish channel, data
@@ -81,8 +86,12 @@ publish = (client, activity_logger) ->
 
 dashboard = ->
   port = 9292 # XXX: hard coded
-  client = new Faye.Client "http://localhost:#{port}/faye",
-    timeout: 60
+  try
+    client = new Faye.Client "http://localhost:#{port}/faye",
+      timeout: 60
+  catch error
+    alert("Faye does not appear to be running")
+    throw error
   console.log(client)
 
   activity_logger = activity_log()
@@ -91,7 +100,7 @@ dashboard = ->
     if message.channel == "/meta/connect"
       msg = "connection status #{message.successful}"
       $("#connection").html(msg)
-      activity_logger.show_message(message, "meta")
+      # activity_logger.show_message(message, "meta")
     else
       if message.data
         extras = {}
@@ -103,8 +112,7 @@ dashboard = ->
     console.log(message)
     callback(message) 
 
-  client.addExtension
-    incoming: incoming_handler
+  client.addExtension({incoming: incoming_handler})
 
   publisher = publish(client, activity_logger)
   publisher.set_input("{}")
--- dashboard.js
+++ dashboard.js
@@ -4,18 +4,32 @@
     var which_msg;
     which_msg = 0;
     return {
-      show_message: function(message, type, subscription) {
-        var msg, td, tr;
+      show_message: function(message, type, channel) {
+        var msg, parent_data, td, tr;
+        if (channel == null) {
+          channel = null;
+        }
         which_msg += 1;
-        msg = JSON.stringify(message, null, 4);
         tr = $('<tr valign="top">');
-        td = $("<td>(" + which_msg + ") " + type + "</td>");
+        parent_data = null;
+        if (message.__parent_data__) {
+          parent_data = message.__parent_data__;
+          message.__parent_data__ = void 0;
+          channel = parent_data.channel;
+        }
+        td = $("<td>");
+        td.append(channel);
         tr.append(td);
-        subscription || (subscription = '&nbsp;');
-        td = $("<td>" + subscription + "</td>");
+        msg = JSON.stringify(message, null, 4);
+        td = $("<td>(" + which_msg + ") " + type + "</td>");
         tr.append(td);
         td = $("<td><pre>" + msg + "</pre></td>");
         tr.append(td);
+        if (parent_data) {
+          msg = JSON.stringify(parent_data, null, 4);
+          td = $("<td><pre>" + msg + "</pre></td>");
+          tr.append(td);
+        }
         $("table#log").prepend(tr);
         return which_msg;
       }
@@ -24,49 +38,40 @@
   set_up_subscribe_form = function(client, activity_logger) {
     var on_subcribe, which_subscription;
     which_subscription = 0;
-    on_subcribe = function() {
-      var button, cancel, channel, form, subscribe_callback, subscription, subscription_label, td, tr;
-      try {
-        which_subscription += 1;
-        channel = $("#subscribe #subscribe_channel").val();
-        subscription_label = "" + which_subscription + " " + channel;
-        subscribe_callback = function(message) {
-          var my_subscription;
-          console.log("got message", message);
-          my_subscription = subscription_label;
-          return activity_logger.show_message(message, 'incoming', my_subscription);
+    on_subcribe = function(evt) {
+      var button, cancel, channel, form, subscribe_callback, subscription, td, tr;
+      evt.preventDefault();
+      which_subscription += 1;
+      channel = $("#subscribe #subscribe_channel").val();
+      subscribe_callback = function(message) {
+        console.log("got message", message);
+        return activity_logger.show_message(message, 'incoming');
+      };
+      console.log("subscribing", channel);
+      subscription = client.subscribe(channel, subscribe_callback);
+      tr = $('<tr valign="top">');
+      td = $("<td id=subscription" + which_subscription + ">");
+      td.append(which_subscription);
+      tr.append(td);
+      td = $("<td>" + channel + "</td>");
+      tr.append(td);
+      form = $("<form id='cancel_subscribe'>");
+      button = $("<input type='submit' value='cancel' />");
+      form.append(button);
+      td = $("<td>");
+      td.append(form);
+      tr.append(td);
+      cancel = function(subscription, tr) {
+        return function(evt) {
+          evt.preventDefault();
+          subscription.cancel();
+          return tr.hide();
         };
-        console.log("subscribing", channel);
-        subscription = client.subscribe(channel, subscribe_callback);
-        tr = $('<tr valign="top">');
-        td = $("<td id=subscription" + which_subscription + ">");
-        td.append(which_subscription);
-        tr.append(td);
-        td = $("<td>" + channel + "</td>");
-        tr.append(td);
-        form = $("<form id='cancel_subscribe'>");
-        button = $("<input type='submit' value='cancel' />");
-        form.append(button);
-        td = $("<td>");
-        td.append(form);
-        tr.append(td);
-        cancel = function(subscription, tr) {
-          return function() {
-            subscription.cancel();
-            tr.hide();
-            return false;
-          };
-        };
-        form.submit(cancel(subscription, tr));
-        $("#subscriptions table").prepend(tr);
-      } catch (error) {
-        console.log("error", error);
-      }
-      return false;
+      };
+      form.submit(cancel(subscription, tr));
+      return $("#subscriptions table").prepend(tr);
     };
-    return $('form#subscribe').click(function() {
-      return on_subcribe();
-    });
+    return $('form#subscribe').submit(on_subcribe);
   };
   publish = function(client, activity_logger) {
     var self;
@@ -76,7 +81,7 @@
       data = self.get_input();
       if (data != null) {
         channel = $("#publish #channel").val();
-        which_msg = activity_logger.show_message(data, 'outgoing', null);
+        which_msg = activity_logger.show_message(data, 'outgoing', channel);
         console.log("showing", which_msg);
         console.log("publishing", channel, data);
         client.publish(channel, data);
@@ -103,9 +108,14 @@
   dashboard = function() {
     var activity_logger, client, incoming_handler, port, publisher;
     port = 9292;
-    client = new Faye.Client("http://localhost:" + port + "/faye", {
-      timeout: 60
-    });
+    try {
+      client = new Faye.Client("http://localhost:" + port + "/faye", {
+        timeout: 60
+      });
+    } catch (error) {
+      alert("Faye does not appear to be running");
+      throw error;
+    }
     console.log(client);
     activity_logger = activity_log();
     incoming_handler = function(message, callback) {
@@ -113,7 +123,6 @@
       if (message.channel === "/meta/connect") {
         msg = "connection status " + message.successful;
         $("#connection").html(msg);
-        activity_logger.show_message(message, "meta");
       } else {
         if (message.data) {
           extras = {};
